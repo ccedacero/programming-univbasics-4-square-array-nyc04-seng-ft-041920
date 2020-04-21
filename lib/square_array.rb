2d49bc154bcd896938a1586531ffa9e970cc9b18
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-21 21:38:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
 i = 0 
 square_array = []
 while i < array.length {
-  square_array.push(array[i]**2)
+  square_array.push(array[i]*array[i])
   i += 1
 }
 return square_array
